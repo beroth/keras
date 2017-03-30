bce21779c069d9b97421d75a514e226eea6b60ba
Benjamin Roth <beroth@cis.uni-muenchen.de>
2017-03-30 17:48:48 +0200

    crf scores

--- keras/layers/crf.py
+++ keras/layers/crf.py
@@ -144,8 +144,10 @@ def _forward(x, reduce_step, initial_states, U, mask=None):
     inputs = K.concatenate([inputs, K.zeros_like(inputs[:, -1:, :, :])], axis=1)
     print("===")
     print(initial_states[0].type.ndim)
+    print(initial_states[0].type.shape)
     last, values, _ = K.rnn(_forward_step, inputs, initial_states)
     print(last.type.ndim)
+    print(last.type.shape)
     return last, values
 
 
